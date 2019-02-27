6cc2b1ce2f0e585a9ed14c920e31cd3bc03978ec
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2019-02-27 14:58:28 +0100

    Adjust matcher

--- src/matchers/toHaveBeenRequestedWith.ts
+++ src/matchers/toHaveBeenRequestedWith.ts
@@ -1,26 +1,40 @@
 import { equals } from 'expect/build/jasmine_utils';
 import diff from 'jest-diff';
-import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+import {
+  matcherHint,
+  printExpected,
+  printReceived,
+  RECEIVED_COLOR,
+} from 'jest-matcher-utils';
 
 export const toHaveBeenRequestedWith = (
   { mock }: jest.Mock<any>,
   expected: object,
 ) => {
-  const received = { ...mock.calls[0][0].request };
-  const pass = equals(received, expected);
+  const calls = mock.calls;
+  const pass = calls.length > 0 && equals(calls[0][0].request, expected);
   return {
     pass,
     message: () => {
-      const diffString = diff(expected, received);
       const hintString = matcherHint('.toHaveBeenRequestedWith');
       const expectedString = printExpected(expected);
-      const receivedString = printReceived(received);
+      const mismatchString = formatMismatchedCalls(mock.calls, expected);
       return (
         `${hintString}\n\n` +
-        `Expected value to equal:\n${expectedString}\n` +
-        `Received:\n${receivedString}\n` +
-        `Difference:\n${diffString}`
+        `Expected request value to equal:\n${expectedString}\n` +
+        mismatchString
       );
     },
   };
+};
+
+const formatMismatchedCalls = (calls, expected): string => {
+  if (!calls.length) {
+    return `But it was ${RECEIVED_COLOR('not called')}.`;
+  }
+
+  const received = calls[0][0].request;
+  const diffString = diff(expected, received);
+  const receivedString = printReceived(received);
+  return `Received:\n${receivedString}\n` + `Difference:\n${diffString}`;
 };
