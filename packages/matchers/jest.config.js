70db556c7c71d593e2104e423859b12a8b871f46
Ievgen Kyrpychenko <evkirpichenko@gmail.com>
2024-09-06 17:15:49 +0200

    fix: TECH disable colors. With colors snapshots are failing on ci

--- packages/matchers/jest.config.js
+++ packages/matchers/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
   transform: { '^.+\\.tsx?$': ['ts-jest', { diagnostics: false }] },
+  maxWorkers: 1,
+  workerIdleMemoryLimit: '1.5GB',
 }
